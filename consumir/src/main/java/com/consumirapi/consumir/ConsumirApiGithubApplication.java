c5482d43e8cafe0a64a55837c86e0a682ac4502d
johnformata <johnvianaluz@gmail.com>
2024-02-20 22:33:42 -0300

    criando projeto para consumir github

---
+++ consumir/src/main/java/com/consumirapi/consumir/ConsumirApiGithubApplication.java
@@ -0,0 +1,13 @@
+package com.consumirapi.consumir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumirApiGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumirApiGithubApplication.class, args);
+	}
+
+}
---
+++ consumir/src/test/java/com/consumirapi/consumir/ConsumirApiGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consumirapi.consumir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumirApiGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
